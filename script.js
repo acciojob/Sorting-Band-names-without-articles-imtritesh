11549bcd7a6505a3b0d921a9ba7248c2562b572a
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-04-15 00:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //your code here
-
+const sortedBandNames = bandNames.sort((a, b) => {
+  const nameA = a.replace(articleRegex, '');
+  const nameB = b.replace(articleRegex, '');
+  return nameA.localeCompare(nameB);
+});
 
 sortedBandNames.forEach(bandName => {
   const li = document.createElement('li');
