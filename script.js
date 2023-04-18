0f2b21183467092713933227da823167d5aebb05
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-04-18 13:29:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ const ans = [];
 for(let i=0; i<arr.length; i++){
   let str = arr[i];
   if(str.startsWith('the' || 'a' || 'an' || 'An' || 'The' || 'A')){
-    ans.unshift(arr[i]);
-  }else{
     ans.push(arr[i]);
+  }else{
+    ans.unshift(arr[i]);
   }
 }
-ans.reverse();
 console.log(ans);
