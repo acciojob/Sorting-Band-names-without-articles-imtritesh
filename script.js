28a51d3abea5ed0616ad207b0a62b0b4581a6f83
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-04-18 13:31:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 const ans = [];
 for(let i=0; i<arr.length; i++){
   let str = arr[i];
-  if(str.startsWith('the' || 'a' || 'an' || 'An' || 'The' || 'A')){
+  if(str.startsWith('An' || 'The' || 'A')){
     ans.push(arr[i]);
   }else{
     ans.unshift(arr[i]);
