645781b128b72f9d090f93e80bbb3e8834cf40ea
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-04-18 14:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const ans = [];
 for(let i=0; i<arr.length; i++){
   let str = arr[i];
   if(str.startsWith('An' || 'The' || 'A')){
-    ans.push(`<li>${str}<li>`);
+    ans.push(`<li>${str}</li>`);
   }else{
-    ans.unshift(`<li>${str}<li>`);
+    ans.unshift(`<li>${str}</li>`);
   }
 }
+let nice =""; ans.forEach(element => nice += element);
+document.getElementById("band").innerHTML = nice;
 console.log(ans);
